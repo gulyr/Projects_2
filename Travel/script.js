0d94a336251ab3d57f7ece4ec8e9e284106ceefd
gulyr <guldastayr@code.edu.az>
2024-11-22 23:05:47 +0400

    feat: "home section added"

--- Travel/script.js
+++ Travel/script.js
@@ -12,8 +12,16 @@ if (navClose) {
   })
 }
 const navLink = document.querySelectorAll('.nav_link')
-const linAction = () => {
+const linkAction = () => {
   const navMenu = document.getElementById('navMenu')
   navMenu.classList.remove('show_menu')
 }
-navLink.forEach((n) => n.addEventListener('click', linAction))
+
+navLink.forEach((n) => n.addEventListener('click', linkAction))
+const blurHeader = () => {
+  const header = document.getElementById('header')
+  this.scrollY >= 50
+    ? header.classList.add('blur_header')
+    : header.classList.remove('blur_header')
+}
+window.addEventListener('scroll', blurHeader)
